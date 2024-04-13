09954e5f814cde0cdd46f87b6ef28cb1a5a6ece7
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-13 18:28:39 +1000

    [backend] feat(api): '/api/customer/membership/subscribe'

--- backend/server.js
+++ backend/server.js
@@ -183,6 +183,44 @@ app.post('/api/customer/edit/password', async (req, res) => {
     }
 });
 
+app.post('/api/customer/membership/subscribe', async (req, res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const customerEmail = authenParts[0];
+    const customerPassword = authenParts[1];
+
+    const sct = req.body.sct;
+
+    if (sct === undefined) {
+        return res.send("Wrong Parameter");
+    }
+
+    const subcribtionType = parseInt(sct);
+
+    if (subcribtionType !== 0 && subcribtionType !== 1) {
+        return res.send("Wrong Type");
+    }
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        await serviceCustomer.authenticateCustomer(customerEmail, customerPassword);
+        const newMembershipDate = await serviceCustomer.customerSubcribeMembership(subcribtionType);
+        res.json({
+            subcribtionType: subcribtionType,
+            membershipEnd: newMembershipDate
+        })
+    } catch(error) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
