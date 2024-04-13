467e1c9b141a9a28b3b23e0da15c44af5f9606dd
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-13 20:23:44 +1000

    [backend] feat(api):'/api/customer/payment/delete'

--- backend/server.js
+++ backend/server.js
@@ -326,6 +326,36 @@ app.post('/api/customer/payment/add', async (req, res) => {
     }
 });
 
+app.post('/api/customer/payment/delete', async (req, res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const customerEmail = authenParts[0];
+    const customerPassword = authenParts[1];
+
+    const paymentCode = req.body.pc;
+
+    if (paymentCode === undefined) {
+        return res.send("Wrong Parameter");
+    }
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        await serviceCustomer.authenticateCustomer(customerEmail, customerPassword);
+        await serviceCustomer.deleteCustomerPaymentMethod(paymentCode);
+
+        res.status(200).send({ message: 'Payment Method Successfully Deleted' });
+    } catch (error) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
