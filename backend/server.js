f2d8df32b0abfe7ed014b7c4ef07d5bce98f3edd
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-13 18:20:13 +1000

    [backend] feat(api): '/api/customer/edit/email'

--- backend/server.js
+++ backend/server.js
@@ -93,6 +93,36 @@ app.get('/api/customer/information', async (req, res) => {
     }
 });
 
+app.post('/api/customer/edit/email', async (req, res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const customerEmail = authenParts[0];
+    const customerPassword = authenParts[1];
+
+    const newEmail = req.body.newEmail;
+
+    if (newEmail === undefined) {
+        return res.send("Wrong Parameter");
+    }
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        await serviceCustomer.authenticateCustomer(customerEmail, customerPassword);
+        await serviceCustomer.setCustomerInformation(newEmail, null, null);
+
+        res.status(200).send({ message: 'Email Successfully Changed' });
+    } catch (error) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
