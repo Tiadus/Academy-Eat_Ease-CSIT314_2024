0f5b99e3c389043ada600ba17011e2168e31a97f
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-13 18:22:53 +1000

    [backend] feat(api): '/api/customer/edit/password'

--- backend/server.js
+++ backend/server.js
@@ -153,6 +153,36 @@ app.post('/api/customer/edit/phone', async (req, res) => {
     }
 });
 
+app.post('/api/customer/edit/password', async (req, res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const customerEmail = authenParts[0];
+    const customerPassword = authenParts[1];
+
+    const newPassword = req.body.newPassword;
+
+    if (newPassword === undefined) {
+        return res.send("Wrong Parameter");
+    }
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        await serviceCustomer.authenticateCustomer(customerEmail, customerPassword);
+        await serviceCustomer.setCustomerInformation(null, null, newPassword);
+
+        res.status(200).send({ message: 'Password Successfully Changed' });
+    } catch (error) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
