96a7b7a06a5e2bfa0124af71a224f144c3761cef
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-13 11:15:56 +1000

    [backend] feat(api): '/api/customer/register'

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,26 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.post('/api/customer/register', async (req,res) => {
+    const body = req.body;
+    const customerEmail = body.customerEmail;
+    const customerName = body.customerName;
+    const customerPhone = body.customerPhone;
+    const customerPassword = body.customerPassword;
+
+    if (customerEmail === undefined || customerName === undefined || customerPhone === undefined || customerPassword === undefined) {
+        return res.send("Wrong Parameter");
+    }
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        const newCustomerCode = await serviceCustomer.registerCustomer(customerEmail, customerName, customerPhone, customerPassword);
+        res.json({customerCode: newCustomerCode});
+    } catch (error) {
+        res.status(error.status).json({error: error.message});
+    }
+})
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
