52fbeda757ef2528c006297b9085509179617b94
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-14 20:28:35 +1000

    [backend] feat(api): '/api/customer/orders/active'
    For viewing active orders

--- backend/server.js
+++ backend/server.js
@@ -629,6 +629,30 @@ app.post('/api/customer/order/create', async (req, res) => {
     }
 });
 
+app.get('/api/customer/orders/active', async (req, res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const customerEmail = authenParts[0];
+    const customerPassword = authenParts[1];
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        await serviceCustomer.authenticateCustomer(customerEmail, customerPassword);
+        const orderCode = await serviceCustomer.getCustomerOrders(0);
+
+        res.json(orderCode);
+    } catch (error) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
