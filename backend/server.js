cd357aff381256f5140f9b74d72528e2af725ddf
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-14 20:59:28 +1000

    [backend] feat(api): '/api/customer/order/view'
    For viewing the detail of an order

--- backend/server.js
+++ backend/server.js
@@ -677,6 +677,33 @@ app.get('/api/customer/orders/history', async (req, res) => {
     }
 });
 
+app.get('/api/customer/order/view', async (req,res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const customerEmail = authenParts[0];
+    const customerPassword = authenParts[1];
+
+    const query = req.query;
+    const orderCode = query.oc;
+
+    try {
+        const serviceCustomer = new ServiceCustomer();
+        await serviceCustomer.authenticateCustomer(customerEmail, customerPassword);
+        const orderInformation = await serviceCustomer.customerViewOrder(orderCode);
+
+        res.json(orderInformation);
+    } catch (error) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
