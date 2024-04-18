6551a9b4ca0508d64ed9396f281fef8282ffff38
Viet Thai <vietthai.nguyen03@gmail.com>
2024-04-18 12:25:18 +1000

    [backend] feat(api): '/api/owner/information'
    Add api for restaurant owner to view their
    information.

--- backend/server.js
+++ backend/server.js
@@ -800,6 +800,29 @@ app.post('/api/owner/login', async (req, res) => {
     }
 });
 
+app.get('/api/owner/information', async (req, res) => {
+    const authen = req.headers.authorization;
+    if (authen === undefined) {
+        return res.send("Server Unavailable");
+    }
+
+    const encodedCredential = authen.split(" ")[1];
+    const decodedCredential = atob(encodedCredential);
+
+    const authenParts = decodedCredential.split(":");
+    const restaurantEmail = authenParts[0];
+    const restaurantPassword = authenParts[1];
+
+    try {
+        const serviceRestaurant = new ServiceRestaurant();
+        await serviceRestaurant.authenticateOwner(restaurantEmail, restaurantPassword);
+        const restaurantInformation = serviceRestaurant.getRestaurantInformation();
+        res.json(restaurantInformation);
+    } catch (errorCode) {
+        res.status(error.status).json({error: error.message});
+    }
+});
+
 server.listen(4000, function() {
     console.log("Listening on port 4000");
 });
